bc655d60a9552d668aa2fee836e68053a3227511
Hassan Rabeti <hassan.rabeti@alertmedia.com>
2017-08-16 14:56:07 -0500

    Added initial implementation of csharp client generated using swagger-codegen

---
+++ csharp/AlertMedia.Client/AlertMediaClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AlertMedia.CodeGen.Api;
+using AlertMedia.CodeGen.Client;
+
+namespace AlertMedia.Client
+{
+    public class AlertMediaClient
+    {
+        private IUserApi _users;
+        public IUserApi Users => _users ?? (_users = new UserApi(Configuration));
+        
+        private IGroupApi _groups;
+        public IGroupApi Groups => _groups ?? (_groups = new GroupApi(Configuration));
+
+        public readonly Configuration Configuration;
+
+        public AlertMediaClient(string clientId = null, string clientSecretKey = null, string server = null) {
+            Configuration = new Configuration {
+                ApiClient = GetApiClient(server),
+                Username = clientId
+                           ?? System.Environment.GetEnvironmentVariable("AM_CLIENT_ID")
+                           ?? throw new ArgumentNullException(nameof(clientId), "You must specify an AlertMedia API clientId and clientSecretKey."),
+                Password = clientSecretKey
+                           ?? System.Environment.GetEnvironmentVariable("AM_CLIENT_SECRET_KEY")
+                           ?? throw new ArgumentNullException(nameof(clientSecretKey), "You must specify an AlertMedia API clientId and clientSecretKey.")
+            };
+        }
+
+        private ApiClient GetApiClient(string address) {
+            if (string.IsNullOrWhiteSpace(address))
+                return new ApiClient();
+            if (address.IndexOf("http", StringComparison.InvariantCultureIgnoreCase) < 0)
+                address = $"https://{address}";
+            address = $"{new Uri(address).AbsoluteUri}api";
+            return new ApiClient(address);
+        }
+    }
+}
---
+++ csharp/Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AlertMedia.Client;
+
+namespace AlertMedia.Example
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            var client = new AlertMediaClient(
+                clientId:"00000000-0000-0000-0000-000000000000", 
+                clientSecretKey:"00000000000000000000",
+                server:"staging.alertmedia.com"
+            );
+            var customerId = 99;
+
+            foreach (var user in client.Users.List(customerId: customerId)) {
+                Console.WriteLine($"{user.FirstName} {user.LastName}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
