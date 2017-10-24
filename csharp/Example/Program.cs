1eeaaaedb6664bf753f2a3562b2dff31bda5c4be
Hassan Rabeti <hassan.rabeti@alertmedia.com>
2017-10-24 17:22:24 -0500

    Added SetPhoto, provided an example and bumped version

--- csharp/Example/Program.cs
+++ csharp/Example/Program.cs
@@ -1,26 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.IO;
 using AlertMedia.Client;
-using AlertMedia.CodeGen.Client;
 
 namespace AlertMedia.Example
 {
     class Program
     {
-        static void Main(string[] args) {
+        private static AlertMediaClient _client;
 
-            var client = new AlertMediaClient(
+        static void Main(string[] args) {
+ 
+            _client = new AlertMediaClient(
                 clientId:"00000000-0000-0000-0000-000000000000", 
                 clientSecretKey:"00000000000000000000"
             );
-
-            foreach (var user in client.Users.List(client.ApiCustomer.Id))
-                Console.WriteLine($"{user.FirstName} {user.LastName}");
+            
+            /* 
+             * Uncomment any the following examples to try out the client
+             */
+            #region Examples
+            // ListUsers();
+            // SetApiUserPhoto("sample.png");
+            #endregion
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
-
         }
+
+        static void ListUsers() {
+            foreach (var user in _client.Users.List(_client.ApiCustomer.Id))
+                Console.WriteLine($"{user.Id}: {user.FirstName} {user.LastName}");
+        }
+
+        static void SetApiUserPhoto(string path) {
+            using (var stream = new FileStream(path, FileMode.Open))
+                _client.Users.SetPhoto(_client.ApiUser.Id, stream);
+        }
+
     }
 }
